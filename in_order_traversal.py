cd8cb5974e1ce8ea2d0095a37588cd7f109a834f
Tochi5050 <ctochi46@gmail.com>
2025-01-06 19:45:40 +0100

    Adding in_order_traversal

---
+++ in_order_traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = [root]
+        final_arr = []
+        if root is None:
+            return final_arr
+
+        while root.left is not None:
+            stack.append(root.left)
+            root = root.left
+        while len(stack) > 0:
+            node = stack.pop()
+
+            if node.right is not None and hasattr(node.right, 'visited') == False:
+                stack.append(node.right)
+                node.right.visited = True
+            if node.left is not None and hasattr(node.left, 'visited') == False:
+                stack.append(node)
+                stack.append(node.left)
+            if node.left is None:
+                final_arr.append(node.val)
+                node.visited = True
+            if node.left is not None and hasattr(node.left, 'visited') == True:
+                final_arr.append(node.val)
+                node.visited = True
+        return final_arr
